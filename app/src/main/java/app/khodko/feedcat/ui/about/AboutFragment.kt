326d5c91da53e6ddc0c32fc0b75f1356c8cc06b5
Aliaksander <45327050+mobilebrainz@users.noreply.github.com>
2022-02-03 08:49:15 +0300

    + raw about

--- app/src/main/java/app/khodko/feedcat/ui/about/AboutFragment.kt
+++ app/src/main/java/app/khodko/feedcat/ui/about/AboutFragment.kt
@@ -1,10 +1,14 @@
 package app.khodko.feedcat.ui.about
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.text.HtmlCompat
 import androidx.fragment.app.Fragment
+import app.khodko.feedcat.R
 import app.khodko.feedcat.core.extension.getViewModelExt
 import app.khodko.feedcat.databinding.FragmentAboutBinding
 
@@ -22,13 +26,29 @@ class AboutFragment : Fragment() {
     ): View {
         aboutViewModel = getViewModelExt { AboutViewModel() }
         _binding = FragmentAboutBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        return root
+        binding.infoView.text = HtmlCompat.fromHtml(getString(R.string.info_view), HtmlCompat.FROM_HTML_MODE_LEGACY)
+        binding.emailBtn.setOnClickListener { sendEmail() }
+
+        return binding.root
+    }
+
+    private fun sendEmail() {
+        try {
+            val intent = Intent(Intent.ACTION_SEND)
+            intent.data = Uri.parse("mailto:")
+            intent.type = "text/plain"
+            intent.putExtra(Intent.EXTRA_EMAIL, arrayOf(getString(R.string.feedcat_email)))
+            intent.putExtra(Intent.EXTRA_SUBJECT, getString(R.string.app_name))
+            startActivity(Intent.createChooser(intent, getString(R.string.choose_email_client)))
+        } catch (ex: android.content.ActivityNotFoundException) {
+            //showInfoSnackbar(R.string.send_mail_error);
+        }
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
+
 }
