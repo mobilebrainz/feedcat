081d1f9a1b78d86360bb0d9c4a65744796228a6a
Aliaksander <45327050+mobilebrainz@users.noreply.github.com>
2022-02-03 11:58:39 +0300

    + splash activity

---
+++ app/src/main/java/app/khodko/feedcat/SplashActivity.kt
@@ -0,0 +1,19 @@
+package app.khodko.feedcat
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+}
